af8f9dbf59899caddb0199c1f9d752dec7fd2b18
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-10-08 22:38:43 +0300

    added post request from react app

--- frontend/test-front/src/components/Auto.js
+++ frontend/test-front/src/components/Auto.js
@@ -4,13 +4,25 @@ export default function Auto() {
     const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
     const[name,setName]=useState('')
     const[comand,setComand]=useState('')
-    const[discr,setDiscr]=useState('')
+    const[discription,setDiscr]=useState('')
     const[students,setStudents]=useState([])
 
+    const handleClick=(e)=>{
+        e.preventDefault()
+        const student={name, comand, discription, url : "dddd"}
+        console.log(student)
+        fetch("http://localhost:8080/student/add",{
+          method:"POST",
+          headers:{"Content-Type":"application/json"},
+          body:JSON.stringify(student)
+    
+      }).then(()=>{
+        console.log("New Student added")
+      })
+    }
+
     useEffect(()=>{
-        console.log("!!!!fetched!!!!")
         fetch("http://localhost:8080/student/getAll")
-        .then(console.log("!!!!fetched_1!!!!"))
         .then(res=>res.json())
         .then((result)=>{
           setStudents(result);
@@ -19,7 +31,7 @@ export default function Auto() {
       },[])
         return (
           <div>
-          <h1>Autos</h1>
+          {/* <h1>Autos</h1>
       
           <div elevation={3} style={paperStyle}>
       
@@ -28,16 +40,25 @@ export default function Auto() {
                Id:{student.id}<br/>
                Name:{student.name}<br/>
                Address:{student.comand}
-      
               </p>
             ))
       }
-      
-      
-          </div>
-      
-      
-      
+          </div> */}
+            <form noValidate autoComplete="off">
+                <input type='text' id="outlined-basic" label="Student Name" variant="outlined" fullWidth 
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                />
+                <input type='text' id="outlined-basic" label="Student Adress" variant="outlined" fullWidth
+                value={comand}
+                onChange={(e)=>setComand(e.target.value)}
+                />
+                <input type='text' id="outlined-basic" label="Student Adress" variant="outlined" fullWidth
+                value={discription}
+                onChange={(e)=>setDiscr(e.target.value)}
+                />
+                <button variant="contained" color="secondary" onClick={handleClick}>Submit</button>
+            </form>    
           </div>
         );
       }
--- fullstack/src/main/java/com/test/fullstack/controller/StudentController.java
+++ fullstack/src/main/java/com/test/fullstack/controller/StudentController.java
@@ -16,4 +16,13 @@ public class StudentController {
     public List<Auto> list(){
         return DataBase.getAutos();
     }
+
+	@PostMapping("/add")
+    public String add(@RequestBody Auto auto){
+		System.out.println("QQQQQQQQQQQQ");
+		System.out.println(auto.getName() + " " + auto.getComand() + " " + auto.getDiscription());
+		System.out.println("QQQQQQQQQQQQ");
+		DataBase.addAuto(auto);
+        return "New student is added";
+    }
 }
